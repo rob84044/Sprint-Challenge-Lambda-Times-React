7f48d1c2c292403cb54977129f74b7e72912d0ad
Robert Pedersen <rob84044@users.noreply.github.com>
2019-06-01 14:56:45 -0600

    Added moment.js so I can have an updated date object displayed at top left of page

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
-
-// Refactor this component to use styled components and not classNames. 
+import Moment from 'react-moment';
+import 'moment-timezone';
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
-
+const dateToFormat = new Date(Date.now('DD MM YYYY'));
 const Header = () => {
   return (
     <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+      <span className="date">
+        {<Moment format="MMMM Do YYYY" date={dateToFormat} />}
+      </span>
       <h1>Lambda Times</h1>
       <span className="temp">98°</span>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
