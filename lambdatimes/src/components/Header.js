747c2cfb26b9aeb795597ebfb7307fda49bff8e6
Robert Pedersen <rob84044@users.noreply.github.com>
2019-06-01 20:32:45 -0600

    Added momentJS, @emotion/styled, and set up styledHeaderJS file and refactored headerJS file as well

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Moment from 'react-moment';
 import 'moment-timezone';
+import styled from '@emotion/styled';
+import {
+  HeaderDiv,
+  HeaderSpanDate,
+  HeaderH1,
+  HeaderSpanTemp
+} from '../components/styledComps/styledHeader';
 // Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
+
 const dateToFormat = new Date(Date.now('DD MM YYYY'));
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">
+    <HeaderDiv>
+      <HeaderSpanDate>
         {<Moment format="MMMM Do YYYY" date={dateToFormat} />}
-      </span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+      </HeaderSpanDate>
+      <HeaderH1>Lambda Times</HeaderH1>
+      <HeaderSpanTemp>98°</HeaderSpanTemp>
+    </HeaderDiv>
   );
 };
 
